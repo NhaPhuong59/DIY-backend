7a336eda4fae440170dfe143cce47df0081a3712
Helen <helen@rexy.tech>
2024-09-24 12:56:49 +0700

    update cors

--- app.js
+++ app.js
@@ -33,6 +33,24 @@ mongoose
 app.use("/api", indexRouter);
 
 app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
   const err = new Error("Not Found");
   err.statusCode = 404;
   next(err);
